b8af911050086b2ffefdc26e29a985c21f2e1c49
Hardi251 <hardi.patel@volansys.com>
2023-04-10 10:50:29 +0530

    Framework addedcd /Users/hardipatel/Documents/SDK_demo/ExampleSDK/ExampleSDK

---
+++ ExampleSDK/ExampleSDK/ExampleSDK.h
@@ -0,0 +1,18 @@
+//
+//  ExampleSDK.h
+//  ExampleSDK
+//
+//  Created by Hardi Patel on 10/04/23.
+//
+
+#import <Foundation/Foundation.h>
+
+//! Project version number for ExampleSDK.
+FOUNDATION_EXPORT double ExampleSDKVersionNumber;
+
+//! Project version string for ExampleSDK.
+FOUNDATION_EXPORT const unsigned char ExampleSDKVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <ExampleSDK/PublicHeader.h>
+
+
---
+++ ExampleSDK/ExampleSDK/SDKService.swift
@@ -0,0 +1,18 @@
+//
+//  SDKService.swift
+//  ExampleSDK
+//
+//  Created by Hardi Patel on 10/04/23.
+//
+
+import Foundation
+
+public class SDKService {
+    
+    public init() {}
+    
+    public static func ReturnableString() -> String {
+        return "This is Demo of SDK creation."
+    }
+    
+}
